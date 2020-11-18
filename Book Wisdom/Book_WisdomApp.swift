db13c791b3b1136ca0761db1802a6088ef2644a6
Igor Bragaia <igor.bragaia@gmail.com>
2020-11-17 23:15:05 -0300

    Initial Commit

---
+++ Book Wisdom/Book_WisdomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_WisdomApp.swift
+//  Book Wisdom
+//
+//  Created by Igor Bragaia on 11/17/20.
+//
+
+import SwiftUI
+
+@main
+struct Book_WisdomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
