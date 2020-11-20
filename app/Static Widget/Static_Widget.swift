4a892c86f483fb2f6b646f6f76e92d32fb18ec07
Igor Bragaia <igor.bragaia@gmail.com>
2020-11-20 01:08:35 -0300

    Add MVP interface

--- app/Static Widget/Static_Widget.swift
+++ app/Static Widget/Static_Widget.swift
@@ -9,40 +9,68 @@ import WidgetKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+    func placeholder(in context: Context) -> QuoteEntry {
+        let quotePlaceholder = Quote(text:"aaa", author: "cde", reference: "fgh")
+        return QuoteEntry(date: Date(), quote: quotePlaceholder )
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
+    func getSnapshot(in context: Context, completion: @escaping (QuoteEntry) -> ()) {
+        let quotePlaceholder = Quote(text:"bbb", author: "cde", reference: "fgh")
+        let entry = QuoteEntry(date: Date(), quote: quotePlaceholder )
         completion(entry)
     }
 
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
+    func getTimeline(in context: Context, completion: @escaping (Timeline<QuoteEntry>) -> ()) {
+        QuoteLoader.fetch { result in
+            let quote: Quote
+            if case Result.success(let fetchedQuote) = result {
+                quote = fetchedQuote
+            } else {
+                quote = Quote(text:"Error", author: "---", reference: "---")
+            }
 
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
-            entries.append(entry)
+            let entry = QuoteEntry(date: Date(), quote: quote )
+            let timeline = Timeline(entries: [entry], policy: .atEnd)
+            completion(timeline)
         }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
     }
 }
 
-struct SimpleEntry: TimelineEntry {
-    let date: Date
+struct QuoteLoader {
+    static var quotes = [
+        Quote(text: "\"You got into this elite institution. Your worries are over. You're set for life\". That's probably the kind of thing that's true only if you don't believe it.", author: "Peter Thiel", reference: "Zero to One")
+    ]
+
+    static func fetch(completion: @escaping (Result<Quote, Error>) -> Void) {
+        completion(.success(quotes[0]))
+    }
+}
+
+struct Quote {
+    let text: String
+    let author: String
+    let reference: String
+}
+
+struct QuoteEntry : TimelineEntry {
+    var date: Date
+    let quote: Quote
 }
 
 struct Static_WidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        Text(entry.date, style: .time)
+        VStack{
+            Text(entry.quote.text)
+                .font(.footnote)
+            Text(entry.quote.reference)
+                .font(.footnote)
+                .italic()
+        }.padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .leading)
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: [.orange, .yellow]), startPoint: .top, endPoint: .bottom))
     }
 }
 
@@ -54,14 +82,15 @@ struct Static_Widget: Widget {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             Static_WidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Book Wisdom")
+        .description("Get inspiration from the quotes best recommended.")
+        .supportedFamilies([.systemMedium, .systemLarge])
     }
 }
 
 struct Static_Widget_Previews: PreviewProvider {
     static var previews: some View {
-        Static_WidgetEntryView(entry: SimpleEntry(date: Date()))
+        Static_WidgetEntryView(entry: QuoteEntry(date: Date(), quote: QuoteLoader.quotes[0]))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
